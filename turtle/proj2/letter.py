57cc455e2a5e50c0aa4a74d8fd9baf8a1a22b677
nanayaa123 <113935321+nanayaa123@users.noreply.github.com>
2023-03-10 14:08:01 -0500

    Update letter.py

--- turtle/proj2/letter.py
+++ turtle/proj2/letter.py
@@ -1,1 +1,18 @@
-#
+import turtle 
+t= turtle.Turtle()
+t.penup()
+t.goto(-30, 50)
+t.pendown()
+t.pensize(10)
+t.pencolor("blue")
+
+t.rt(90)
+t.fd(150)
+
+t.goto(-30, -20)
+t.left(45)
+t.fd(100)
+
+t.goto(-30,-20)
+t.lt(90)
+t.fd(100)
