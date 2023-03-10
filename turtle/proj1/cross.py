b225316dc53a5c3a054bf0c5933084519da0d10f
nanayaa123 <113935321+nanayaa123@users.noreply.github.com>
2023-03-10 14:09:47 -0500

    Update cross.py

--- turtle/proj1/cross.py
+++ turtle/proj1/cross.py
@@ -1,1 +1,24 @@
-#code here
+import turtle 
+t = turtle.Turtle()
+t.color("pink")
+t.begin_fill()
+
+t.penup()
+t.forward(50)
+t.pendown()
+
+def draw_side():
+  t.fd(100)
+  t.rt(90)
+  t.fd(100)
+  t.rt(90)
+  t.fd(100)
+  t.lt(90)
+  
+  
+for x in range(4):
+  draw_side()
+  
+t.end_fill()
+  
+e here
